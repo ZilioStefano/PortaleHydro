74ffd6178a2295022d99ba2a1e7809069ef15261
David Geronazzo <david.geronazzo@zilioenvironment.com>
2024-01-11 17:06:48 +0100

    update:

--- view_portate/views.py
+++ view_portate/views.py
@@ -4,69 +4,96 @@ from django.contrib.auth.decorators import login_required
 import folium
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from statistics import mean
+import numpy as np
+
 
 def createPlot(data, name):
-    if name == "Merone3":
-        yMax = 150
-        name = "Merone III salto"
-    elif name == "Merone1":
-        yMax = 300
-        name = "Merone I salto"
-    else:
-        yMax = 150
-
     t = data["t"]
-    # t = pd.to_datetime(t, format='%d/%m/%Y %H:%M:%S')
-    # Q = np.array(data["Q"])
     Q = data["Q"]
-    fig1 = px.line(x=t, y=Q, template="ggplot2")
-    fig1.update_layout(yaxis_range=[0, yMax], xaxis_title="", yaxis_title="Portata [l/s]", paper_bgcolor='whitesmoke',
-                       height=800)
-    dataPlot = fig1.to_html(name + ".html")
+    Q1 = Q.quantile(0.25)
+    Q3 = Q.quantile(0.75)
+    IQR = Q3 - Q1
+    upper = Q3 + 3 * IQR
+    lower = max([Q1 - 3 * IQR, 0])
+    upper_array = np.where(Q >= upper)[0]
+    lower_array = np.where(Q <= lower)[0]
+
+    data.drop(index=list(upper_array), inplace=True)
+    data.drop(index=list(lower_array), inplace=True)
+
+    tt = data["t"]
+    QQ = data["Q"]
+    maxy = QQ.max()*1.2
+
+    fig = go.Figure()
+
+    fig1 = go.Scatter(x=t, y=Q)
+    fig2 = go.Scatter(x=tt, y=QQ)
+    fig.update_layout(
+        template="ggplot2",
+        yaxis_range=[0, maxy],
+        xaxis_title="",
+        yaxis_title="Portata [l/s]",
+        paper_bgcolor='whitesmoke',
+        height=800)
+    fig.update_xaxes(nticks=10)
+    fig.add_trace(fig1)
+    fig.add_trace(fig2)
+
+
+
+
+
+    fig2 = px.line(x=tt, y=QQ, template="ggplot2")
+    fig2.update_layout(
+        yaxis_range=[0, maxy ],
+        xaxis_title="",
+        yaxis_title="Portata [l/s]",
+        paper_bgcolor='whitesmoke',
+        height=800)
+    fig2.update_xaxes(nticks=10)
+
+
+    dataPlot = fig.to_html(name + ".html")
 
     return dataPlot
 
 
 def createHistogram(data, Name):
 
-    if Name == "Merone3":
-        xMax = 120
-        xMin = 0
-        Name = "Merone III salto"
-    elif Name == "Merone1":
-        xMax = 160
-        xMin = 0
-        Name = "Merone I salto"
-    else:
-        xMax = 150
-        xMin = -10
 
     Q = data["Q"]
-
-    fig1 = px.histogram(Q) #, template="ggplot2", title=Name)
-    fig1.update_layout(xaxis_range=[xMin, xMax], xaxis_title="Portata [ l/s ]", yaxis_title="Conteggi [minuti]", paper_bgcolor='whitesmoke', bargap=0.1, height=500)
-    dataPlot = fig1.to_html("Histo"+Name+".html")
+    fig = px.histogram(Q)
+    fig.update_layout(
+        xaxis_title="Portata [ l/s ]",
+        yaxis_title="Conteggi [minuti]",
+        paper_bgcolor='whitesmoke',
+        bargap=0.1,
+        height=500)
+    dataPlot = fig.to_html("Histo"+Name+".html")
 
     return dataPlot
 
+def createMap(data):
+    Lat = data["Latitudine"]
+    Long = data["Longitudine"]
 
-@login_required
-def home(request):
-    MeasureList = pd.read_excel("view_portate/static/data/Misuratori installati.xlsx")
-    Lat = MeasureList["Latitudine"]
-    Long = MeasureList["Longitudine"]
-    # user_name = request.user.username
-    if request.user.username=='damiano':
-        punto_misura=MeasureList["Punto di misura"]
-    else:
-        punto_misura = MeasureList.loc[MeasureList["Permesso"]==request.user.username,"Punto di misura"].tolist()
+    ### VISUALIZZAZIONE MISURATORI DIFFERENZIATA PER UTENTE ###
+    # if request.user.username=='damiano':
+    #     punto_misura=MeasureList["Punto di misura"]
+    punto_misura = data["Punto di misura"]
+    # else:
+    # punto_misura = MeasureList.loc[MeasureList["Permesso"]==request.user.username,"Punto di misura"].tolist()
 
     MeanLat = mean(Lat)
     MeanLong = mean(Long)
 
     figure = folium.Figure()
-    map = folium.Map(location=[MeanLat, MeanLong], zoom_start=9, control_scale=True,
+    map = folium.Map(location=[MeanLat, MeanLong],
+                     zoom_start=7,
+                     control_scale=True,
                      tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                      attr='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
                      )
@@ -77,10 +104,18 @@ def home(request):
             icon=folium.Icon(icon='fa-weight-scale', prefix='fa')).add_to(map)
 
     map.add_to(figure)
-    # figure.render()
-    figure2 = map._repr_html_()
+    map_fig = map._repr_html_()
+    return map_fig
 
-    context = {"Map": figure2}
+
+
+@login_required
+def home(request):
+    Measures = pd.read_excel("view_portate/static/data/Misuratori installati.xlsx")
+    map_fig = createMap(Measures)
+    punto_misura = Measures["Punto di misura"]
+
+    context = {"Map": map_fig, "punto_misura": punto_misura}
     return render(request, 'view_portate/HomePage.html', context)
 
 @login_required
