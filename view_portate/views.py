5657adf121dd23051b706c62b74c3ee95fa6f7ac
David Geronazzo <david.geronazzo@zilioenvironment.com>
2024-01-15 17:56:03 +0100

    update:

--- view_portate/views.py
+++ view_portate/views.py
@@ -1,72 +1,83 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-import datetime
 import folium
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from statistics import mean
 import numpy as np
-import re
 
-def createPlot(data, name):
-    t = data["t"]
-    Q = data["Q"]
-    Q1 = Q.quantile(0.25)
-    Q3 = Q.quantile(0.75)
-    IQR = Q3 - Q1
-    upper = Q3 + 3 * IQR
-    lower = max([Q1 - 3 * IQR, 0])
-    upper_array = np.where(Q >= upper)[0]
-    lower_array = np.where(Q <= lower)[0]
+colori={"bluscuro":"#30415d",#0bluscuro
+        "blu":"#004fa3",#1blu
+        "bluchiaro":"#88a4ca",#2bluchiaro
+        "arancione":"#ffa700",#3arancione
+        "grigio":"#6a7782",#4grigio
+        "rosso":"#ff7376",#5rosso
+        "azzurro":"#b6dae0",#6azzurro
+        "verde":"#a5c357",#7verde
+        "arancione2":"#ff9c0d",#8arancione2
+        "giallo":"#f2cf65",#9giallo
+        }
 
-    data.drop(index=list(upper_array), inplace=True)
-    data.drop(index=list(lower_array), inplace=True)
+colors=['lightgray', #0
+        'pink', #1
+        'darkred', #2
+        'red', #3
+        'lightblue', #4
+        'darkblue', #5
+        'darkpurple', #6
+        'green', #7
+        'blue', #8
+        'lightgreen', #9
+        'lightred', #10
+        'gray', #11
+        'cadetblue', #12
+        'orange', #13
+        'purple', #14
+        'darkgreen', #15
+        'black', #16
+        'beige',  #17
+        'white' #18
+        ]
 
-    tt = data["t"]
-    QQ = data["Q"]
-    maxy = QQ.max()*1.2
+def createPlot(data, name, column):
+    t = pd.to_datetime(data['t'], format='%d/%m/%Y,%H:%M:%S')
+    Q_raw = data.iloc[:,1]
+    Q_filtered = data.iloc[:,column[2]]
+    Q_smooth = data.iloc[:,column[3]]
+
+    max_y = max(Q_raw)
+    min_y = min(Q_raw)
+    range = max_y - min_y
 
     fig = go.Figure()
 
-    fig1 = go.Scatter(x=t, y=Q)
-    fig2 = go.Scatter(x=tt, y=QQ)
+    plot1 = go.Scatter(x=t, y=Q_raw)
+    plot2 = go.Scatter(x=t, y=Q_filtered)
+    plot3 = go.Scatter(x=t, y=Q_smooth)
+
+    fig.add_trace(plot1)
+    fig.add_trace(plot2)
+    fig.add_trace(plot3)
+
     fig.update_layout(
         template="ggplot2",
-        yaxis_range=[0, maxy],
+        yaxis_range=[min_y - 1 / 3 * range, max_y + 1 / 3 * range],
         xaxis_title="",
         yaxis_title="Portata [l/s]",
         paper_bgcolor='whitesmoke',
-        height=800)
-    fig.update_xaxes(nticks=10)
-    fig.add_trace(fig1)
-    fig.add_trace(fig2)
-
-
-
-
-
-    fig2 = px.line(x=tt, y=QQ, template="ggplot2")
-    fig2.update_layout(
-        yaxis_range=[0, maxy ],
-        xaxis_title="",
-        yaxis_title="Portata [l/s]",
-        paper_bgcolor='whitesmoke',
-        height=800)
-    fig2.update_xaxes(nticks=10)
-
+        height=800,
+        colorway=[colori.get("grigio"), colori.get("bluchiaro"), colori.get("arancione")]
+        )
 
     dataPlot = fig.to_html(name + ".html")
 
     return dataPlot
 
-
-def createHistogram(data, Name):
-
-
-    Q = data["Q"]
-    fig = px.histogram(Q)
+def createHistogram(data, Name, column):
+    Q_filtered = data.iloc[:,column[2]]
+    fig = px.histogram(Q_filtered)
     fig.update_layout(
         xaxis_title="Portata [ l/s ]",
         yaxis_title="Conteggi [minuti]",
@@ -81,55 +92,50 @@ def createMap(data):
 
     Lat = data["Latitudine"]
     Long = data["Longitudine"]
+    MeanLat = mean(Lat)
+    MeanLong = mean(Long)
+    Stato = data['Stato']
 
     ### VISUALIZZAZIONE MISURATORI DIFFERENZIATA PER UTENTE ###
     # if request.user.username=='damiano':
     #     punto_misura=MeasureList["Punto di misura"]
-    punto_misura = data["Punto_di_misura"]
+    punti_misura = data["Punto_di_misura"]
     # else:
     # punto_misura = MeasureList.loc[MeasureList["Permesso"]==request.user.username,"Punto di misura"].tolist()
 
-    MeanLat = mean(Lat)
-    MeanLong = mean(Long)
-
-    Stato = data['Stato']
-
     figure = folium.Figure()
     map = folium.Map(location=[MeanLat, MeanLong],
-                     zoom_start=6,
+                     zoom_start=7,
                      control_scale=True,
                      tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                      attr='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
                      )
     folium.TileLayer(tiles='OpenStreetMap', opacity=0.4).add_to(map)
 
-    for i in range(len(punto_misura)):
-
+    #aggiunge icona
+    for i in range(len(punti_misura)):
         if Stato[i] == "In servizio":
-            MarkerColor = "blue"
+            MarkerColor = colors[8]
 
         elif Stato[i] == "Guasto":
-            MarkerColor = "red"
+            MarkerColor = colors[3]
 
         elif Stato[i] == "Da installare":
-            MarkerColor = "green"
+            MarkerColor = colors[13]
 
         elif Stato[i] == "In valutazione":
-            MarkerColor = "orange"
+            MarkerColor = colors[16] #black
 
-        folium.Marker(location=[Lat[i], Long[i]], tooltip=punto_misura[i], max_width=2000,
+        folium.Marker(location=[Lat[i], Long[i]], tooltip=punti_misura[i], max_width=2000,
             icon=folium.Icon(icon='fa-weight-scale', prefix='fa', color=MarkerColor)).add_to(map)
 
     map.add_to(figure)
     map_fig = map._repr_html_()
     return map_fig
 
-
 def ff(x):
     if len(str(x)) <= 3: return 1
     else: return 0
-
-
 def create_feed(MisuratoriTab):
 
     ModelloClean = MisuratoriTab["Modello"].values
@@ -186,45 +192,44 @@ def create_feed(MisuratoriTab):
 
     # Bancale.to_html("OpenBancale.html")
 
-
-
 @login_required
 def home(request):
-    Measures = pd.read_excel("view_portate/static/data/Misuratori installati.xlsx")
-    map_fig = createMap(Measures)
-    FeedHTML = create_feed(Measures)
-    diz = Measures.to_dict(orient="index").items()
+    df_tab_misuratori = pd.read_excel("view_portate/static/data/Misuratori installati.xlsx")
+    df_tab_misuratori = df_tab_misuratori.replace(np.nan, '', regex=True) #sostituisce nan con stringaq vuota
 
-    punto_misura = Measures["Punto_di_misura"]
+    map_fig = createMap(df_tab_misuratori)
+    feed_dict = df_tab_misuratori.to_dict(orient="index").items()
 
-
-    context = {"Map": map_fig, "punto_misura": punto_misura, "Tab": FeedHTML, "diz": diz}
+    context = {"Map": map_fig, "tab_feed": feed_dict, "colori": colori}
     return render(request, 'view_portate/HomePage.html', context)
 
 @login_required
 def merone1(request):
-    data = pd.read_csv("view_portate/static/data/PortateMerone1.csv")
-    graph = createPlot(data, 'Merone1')
-    histo = createHistogram(data, 'Merone1')
-    graphs = {'Graph': graph, 'Histo': histo, 'title': 'Merone I salto'}
+    data = pd.read_csv("view_portate/static/data/datiMerone1.csv")
+    colonne = [0, 1, 2, 3]
+    graph = createPlot(data, 'Merone1', colonne)
+    histo = createHistogram(data, 'Merone1', colonne)
+    graphs = {'Graph': graph,'Histo': histo,'title': 'Merone I salto',"colori": colori}
 
     return render(request, template_name='view_portate/PaginaDati.html', context=graphs)
 
 @login_required
 def merone3(request):
-    data = pd.read_csv("view_portate/static/data/PortateMerone3.csv")
-    graph = createPlot(data, 'Merone3')
-    histo = createHistogram(data, 'Merone3')
-    graphs = {'Graph': graph, 'Histo': histo, 'title': 'Merone III salto'}
+    data = pd.read_csv("view_portate/static/data/datiMerone3.csv")
+    colonne = [0, 1, 2, 3]
+    graph = createPlot(data, 'Merone3', colonne)
+    histo = createHistogram(data, 'Merone3', colonne)
+    graphs = {'Graph': graph,'Histo': histo,'title': 'Merone III salto',"colori": colori}
 
     return render(request, template_name='view_portate/PaginaDati.html', context=graphs)
 
 @login_required
 def trebisacce(request):
-    data = pd.read_csv("view_portate/static/data/PortateTrebisacce.csv")
-    graph = createPlot(data, 'Trebisacce')
-    histo = createHistogram(data, 'Trebisacce')
-    graphs = {'Graph': graph, 'Histo': histo, 'title': 'Partitore Trebisacce'}
+    colonne = [0, 1, 3, 4]
+    data = pd.read_csv("view_portate/static/data/datiTrebisacce.csv")
+    graph = createPlot(data, 'Trebisacce', colonne)
+    histo = createHistogram(data, 'Trebisacce', colonne)
+    graphs = {'Graph': graph,'Histo': histo,'title': 'Partitore Trebisacce',"colori": colori}
 
     return render(request, template_name='view_portate/PaginaDati.html', context=graphs)
 
